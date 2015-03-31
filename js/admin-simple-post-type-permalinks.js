68ae71212d24e31376c73c25fd377de571851762
Hiroshi Urabe <mail@torounit.com>
2015-03-31 21:02:00 +0900

    とりあえず、radioで選択したモノは表示。

--- js/admin-simple-post-type-permalinks.js
+++ js/admin-simple-post-type-permalinks.js
@@ -1,4 +1,11 @@
 (function($){
+    $(".sptp-fieldset").each(function(){
 
+        var $fieldset = $(this);
+        var $customInput = $fieldset.find('input[type=text]');
+        $fieldset.find('input[type=radio]').change(function() {
+            $customInput.val( $(this).val());
+        });
+    });
 
 }(jQuery));
