6a779e66229618e49f0bd898e215ff6e34b2c2d1
Hiroshi Urabe <mail@torounit.com>
2015-04-01 16:42:39 +0900

    test code

---
+++ tests/test-permalink.php
@@ -0,0 +1,64 @@
+<?php
+
+class SPTP_Permalink_Test extends WP_UnitTestCase {
+
+	public function setUp() {
+
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		parent::setUp();
+
+		$wp_rewrite->init();
+		$wp_rewrite->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%' );
+		create_initial_taxonomies();
+
+		update_option( 'page_comments', true );
+		update_option( 'comments_per_page', 5 );
+
+	}
+
+	public function structure_provider() {
+		return array(
+			array("%post_id%"),
+			array("%postname%"),
+			array("%post_id%.html"),
+			array("%postname%.html"),
+		);
+	}
+
+	/**
+	 *
+	 * @test
+	 * @group permalink
+	 * @dataProvider structure_provider
+	 */
+	public function get_permalink( $structure ) {
+
+
+		$post_type = rand_str(12);
+		register_post_type( $post_type,
+			array(
+				"public"     => true,
+				"permalink_structure" => $post_type.'/'.$structure,
+			)
+		);
+
+		$id = $this->factory->post->create( array( 'post_type' => $post_type ) );
+		$this->factory->comment->create_post_comments( $id, 50 );
+
+		do_action('wp_loaded');//fire SPTP_Rewrite::register_rewrite_rules
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+
+		$this->assertEquals( $id, url_to_postid( get_permalink( $id ) ) );
+		$this->go_to( get_permalink( $id ) );
+
+		$this->assertQueryTrue( "is_single", "is_singular" );
+		$this->assertEquals( $post_type, get_post_type() );
+
+		$this->go_to(get_comments_pagenum_link( 2 ) );
+		$this->assertEquals( get_query_var( "cpage"), 2 );
+
+	}
+}
