2c56f4add173d8c3b54bccb440611da757436808
Hiroshi Urabe <mail@torounit.com>
2015-06-26 15:44:11 +0900

    テストを分解

--- tests/test-rewrite.php
+++ tests/test-rewrite.php
@@ -1,6 +1,6 @@
 <?php
 
-class SPTP_Permalink_Test extends WP_UnitTestCase {
+class SPTP_Rewrite_Test extends WP_UnitTestCase {
 
 	public function setUp() {
 
@@ -29,7 +29,7 @@ class SPTP_Permalink_Test extends WP_UnitTestCase {
 	/**
 	 *
 	 * @test
-	 * @group permalink
+	 * @group rewrite
 	 * @dataProvider structure_provider
 	 */
 	public function test_permalink( $structure ) {
@@ -44,21 +44,30 @@ class SPTP_Permalink_Test extends WP_UnitTestCase {
 		);
 
 		$id = $this->factory->post->create( array( 'post_type' => $post_type ) );
+		$this->factory->comment->create_post_comments( $id, 50 );
 
 		do_action('wp_loaded');//fire SPTP_Rewrite::register_rewrite_rules
 		/** @var WP_Rewrite $wp_rewrite */
 		global $wp_rewrite;
 		$wp_rewrite->flush_rules();
 
-		$this->assertEquals( $id, url_to_postid( get_permalink( $id ) ) );
+
+		$this->go_to( get_permalink( $id ) );
+
+		$this->assertQueryTrue( "is_single", "is_singular" );
+		$this->assertEquals( $post_type, get_post_type() );
+
+		$this->go_to( get_comments_pagenum_link( 2 ) );
+		$this->assertEquals( get_query_var( "cpage"), 2 );
 
 	}
 
 	/**
 	 * @test
-	 * @group permalink
+	 * @group rewrite
 	 */
 	public function test_post_type_link() {
+
 
 		$post_type = rand_str(12);
 		$slug = rand_str(12);
@@ -70,7 +79,16 @@ class SPTP_Permalink_Test extends WP_UnitTestCase {
 			)
 		);
 
-		$this->assertEquals( home_url( $slug ), get_post_type_archive_link( $post_type ) );
+		$id = $this->factory->post->create( array( 'post_type' => $post_type ) );
+
+		do_action('wp_loaded');//fire SPTP_Rewrite::register_rewrite_rules
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+		$this->go_to( get_post_type_archive_link( $post_type ) );
+		$this->assertQueryTrue( 'is_post_type_archive', 'is_archive'  );
+		$this->assertEquals( $post_type, get_post_type() );
+
 
 	}
 }
