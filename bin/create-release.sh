49d5533808d0f404a95cd803c0d7b63236fbda2b
Hiroshi Urabe <mail@torounit.com>
2020-07-30 17:40:54 +0900

    update dev envriomnent

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -2,17 +2,23 @@
 
 set -e
 
-if [ $# -lt 1 ]; then
-	echo "usage: $0 <version>"
+if [ $# -lt 2 ]; then
+	echo "usage: $0 <plugin-name> <version>"
 	exit 1
 fi
 
-version=$1
+pluginname=$1
+version=$2
 
 if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
-  sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
+	sed -i.bak -e "s/^Stable tag: .*/Stable tag:        ${version}/g" README.md;
+	rm README.md.bak
 fi
 
+sed -i.bak -e "s/^ \* Version: .*/ * Version: ${version}/g" ${pluginname}.php;
+sed -i.bak -e "s/^ \* @version .*/ * @version ${version}/g" ${pluginname}.php;
+rm ${pluginname}.php.bak
 
-sed -i '' -e "s/^ \* Version: .*/ * Version: ${version}/g" simple-post-type-permalinks.php;
-sed -i '' -e "s/^ \* @version .*/ * @version ${version}/g" simple-post-type-permalinks.php;
+rsync -a --exclude-from=.distignore ./ ./distribution/
+
+rm -rf build
